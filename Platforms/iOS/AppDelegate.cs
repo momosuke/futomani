ebd780a0d21df7dcce2e89e80b40e6babc0a2bfc
YumenoNorimitsu <fusukemomo@gmail.com>
2025-01-18 01:57:25 +0900

    First deploy

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FutomaniApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
